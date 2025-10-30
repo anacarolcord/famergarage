8ac41002b9a67014725219046a84c112b26fe8d9
Murilo <muriloforterodrigues2116@gmail.com>
2025-10-30 18:28:00 -0300

    Refactor ConnectionFactory for improved readability and error handling comments

--- src/main/java/famergarage/dao/ConnectionFactory.java
+++ src/main/java/famergarage/dao/ConnectionFactory.java
@@ -4,15 +4,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import javax.management.RuntimeErrorException;
-
 public class ConnectionFactory {
+    // URL de conexão com o banco de dados SQLite
     private static final String URL = "jdbc:sqlite:agendamento_servicos.db";
 
     public static Connection getConnection() {
         try {
+            // Estabelecendo e retornando a conexão com o banco de dados
             return DriverManager.getConnection(URL);
         } catch (SQLException e) {
+            // Tratamento de erro na conexão
             throw new RuntimeException("Falha ao conectar com o Banco de Dados.", e);
         }
     }
